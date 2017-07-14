6a4ff7c499fb9d91b5a88ce1e95b70711b00d77a
Rian Josua Masikome <rjmasikome@gmail.com>
2017-07-14 14:52:03 +0200

    First push

---
+++ index.js
@@ -0,0 +1,16 @@
+const main = require("./lib/main");
+
+// module.exports = (config = {}) => {
+
+  const config = {};
+  const dbConfig = {
+    mongoUrl: config.mongoUrl || "mongodb://localhost:27017/",
+    db: config.db || config.database || "log-hogger",
+    collection: config.collection || "logs",
+    concat: config.concat || true
+  };
+
+  main(dbConfig);
+
+  console.log("Test", "Yasss", "Basic");
+// }
